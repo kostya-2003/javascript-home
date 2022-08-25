39c426d2134f8ef34f8fe2b86803ae324174721b
Kostya Haxverdyan <kkkk.hhhh.03@mail.ru>
2022-08-25 17:47:33 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+"Use strict";
+
+const btn = document.querySelector("button");
+const mobileMenu = document.querySelector(".mobile_menu");
+const menuList = document.querySelector(".mobile_menu > ul");
+
+btn.addEventListener("click", () => {
+    setTimeout(
+        mobileMenu.style.cssText = `
+            width: 100%;
+            height: 100vh;
+        `,
+        menuList.style.cssText =   `
+            margin: auto;
+            left: 0;
+            right: 0;
+            opacity: 1;
+        `,
+    100
+    )
+    
+})
+
+mobileMenu.addEventListener("click", (e) => {
+    setTimeout(
+        mobileMenu.style.cssText = `
+        width: 0;
+    `,
+    menuList.style.cssText =   `
+        right: -100%;
+        opacity: 0;
+    `,
+    100
+    )
+
+    if(e.target.nodeName === "UL"){
+        mobileMenu.style.cssText = `
+        width: 100%;
+        height: 100vh;
+        `
+    }
+})
+
